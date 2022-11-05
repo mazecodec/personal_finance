69a3d80cbae3bb697bd1cacc73c5d6ee7c25b002
Mazecode <diego.feliud@gmail.com>
2022-11-05 14:46:43 +0100

    Init repository

---
+++ src/test/java/cl/mazecode/personalfinance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mazecode.personalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
